00d200dcbc3268eef62ef91f0d8b29af087262d8
willevans9 <willevans9@gmail.com>
2017-09-06 16:24:19 +0100

    Update pyth-tcp.py

--- pyth-tcp.py
+++ pyth-tcp.py
@@ -1,21 +1,21 @@
-   1 #!/usr/bin/env python
-   2 
-   3 import socket
-   4 
-   5 
-   6 TCP_IP = '127.0.0.1'
-   7 TCP_PORT = 5005
-   8 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
-   9 
-  10 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  11 s.bind((TCP_IP, TCP_PORT))
-  12 s.listen(1)
-  13 
-  14 conn, addr = s.accept()
-  15 print 'Connection address:', addr
-  16 while 1:
-  17     data = conn.recv(BUFFER_SIZE)
-  18     if not data: break
-  19     print "received data:", data
-  20     conn.send(data)  # echo
-  21 conn.close()
+#!/usr/bin/env python
+
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print 'Connection address:', addr
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print "received data:", data
+    conn.send(data)  # echo
+conn.close()
